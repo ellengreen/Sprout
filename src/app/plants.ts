b2ca310bc62c89c125f104de14f9f5862c826e79
Ellen <eegreen.95@gmail.com>
2020-03-22 19:14:49 -0500

    added plants to Plants array

--- src/app/plants.ts
+++ src/app/plants.ts
@@ -1,2 +1,23 @@
-export interface Plants {
-}
+export const Plants = [
+    {
+        name: 'ZZ Plant',
+        location: 'Living Room',
+        light: 'low light',
+        water: 'bi-weekly',
+        date: 'June 2019'
+    },
+    {
+        name: 'Cacti',
+        location: 'Bedroom',
+        light: 'direct light',
+        water: 'monthly',
+        date: 'August 2019'
+    },
+    {
+        name: 'Knives',
+        location: 'Bedroom',
+        light: 'direct light',
+        water: 'monthly',
+        date: 'August 2019'
+    }
+];
