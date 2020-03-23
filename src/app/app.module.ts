d67cc285c3053962b9c654e32404f1ce2bcaa239
Ellen <eegreen.95@gmail.com>
2020-03-22 19:06:45 -0500

    created components for home, plant list, plant details

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { PlantListComponent } from './plant-list/plant-list.component';
+import { PlantDetailsComponent } from './plant-details/plant-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    PlantListComponent,
+    PlantDetailsComponent
   ],
   imports: [
     BrowserModule,
