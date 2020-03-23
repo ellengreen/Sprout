aeebce4d19c26efb6b77f329ed08fd197a1b20c5
Ellen <eegreen.95@gmail.com>
2020-03-22 22:51:25 -0500

    created and populated Marker objects for map

---
+++ src/app/explore-shops/marker.ts
@@ -0,0 +1,42 @@
+export const Marker = [
+    {
+        label: 'Flowers and Weeds',
+        lat: 38.594959,
+        lng: -90.236951,
+        address: ['3201 Cherokee St', ' St. Louis, MO', ' 63118'],
+        phone: '(314) 776-2887',
+        website: 'flowersandweeds.com'
+    },
+    {
+        label: 'Bowood Farms',
+        lat: 38.649378,
+        lng: -90.257176,
+        address: ['4605 Olive St', ' St. Louis, MO', ' 63108'],
+        phone: '(314) 454-6868',
+        website: 'bowoodfarms.com'
+    },
+    {
+        label: "Bayer's Garden Shop",
+        lat: 38.600708,
+        lng: -90.292303,
+        address: ['3401 Hampton Ave', ' St. Louis, MO', ' 63139'],
+        phone: '(314) 781-2314',
+        website: 'bayergardenshops.com'
+    },
+    {
+        label: "Garden Heights Nursery",
+        lat: 38.624694,
+        lng: -90.321087,
+        address: ['1605 S Big Bend Blvd #2207', ' St. Louis, MO', ' 63117'],
+        phone: '(314) 645-7333',
+        website: 'gardenheights.com'
+    },
+    {
+        label: "Maypop Coffee & Garden Shop",
+        lat: 38.604209,
+        lng: -90.337164,
+        address: ['803 Marshall Ave', ' Webster Groves, MO', ' 63119'],
+        phone: '(314) 764-2140',
+        website: 'maypopshop.com'
+    }
+];
