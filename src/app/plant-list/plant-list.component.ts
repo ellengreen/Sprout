832a839a2c334289dc2efdd6212ba079bff1f7f7
Ellen <eegreen.95@gmail.com>
2020-03-22 19:17:05 -0500

    populated plant-list view with lists of plants, organized by their location. router in place to go to details

--- src/app/plant-list/plant-list.component.ts
+++ src/app/plant-list/plant-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Plants } from '../plants';
 
 @Component({
   selector: 'app-plant-list',
@@ -7,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PlantListComponent implements OnInit {
 
+  plants = Plants;
   constructor() { }
 
   ngOnInit() {
