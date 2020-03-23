d54866ba5ea85bc0d565871c98d6427d2c6cf998
Ellen <eegreen.95@gmail.com>
2020-03-22 19:12:58 -0500

    routing for /home & /plants

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { PlantListComponent } from './plant-list/plant-list.component';
+import { PlantDetailsComponent } from './plant-details/plant-details.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', component: HomeComponent
+  },
+  {
+    path: 'plants', component: PlantListComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
