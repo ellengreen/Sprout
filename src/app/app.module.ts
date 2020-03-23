b92180d13ac9aed129c46a90db519c9c43b61113
Ellen <eegreen.95@gmail.com>
2020-03-22 22:53:56 -0500

    installed agm core packages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { PlantListComponent } from './my-plants/plant-list/plant-list.component';
+import { MapViewComponent } from './explore-shops/map-view/map-view.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    PlantListComponent
+    PlantListComponent,
+    MapViewComponent
   ],
   imports: [
     BrowserModule,
