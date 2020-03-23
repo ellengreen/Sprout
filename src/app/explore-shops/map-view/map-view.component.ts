db97c69beaf74e53e195b5f70d9b2d40a318cec9
Ellen <eegreen.95@gmail.com>
2020-03-22 22:59:18 -0500

    made onSelect method

--- src/app/explore-shops/map-view/map-view.component.ts
+++ src/app/explore-shops/map-view/map-view.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Marker } from '../marker';
 
 @Component({
   selector: 'app-map-view',
@@ -7,6 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MapViewComponent implements OnInit {
 
+  //initial center position for map
+  lat = 38.627810;
+  lng = -90.196091;
+  zoom = 11;
+
+  marker = Marker; 
+  selectedMarker; 
+
+  onSelect(marker) : void {
+    this.selectedMarker = marker;
+  }
+  
   constructor() { }
 
   ngOnInit() {
